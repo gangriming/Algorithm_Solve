92d660d2cf5765138c3e149fd57cfa814b2b84d3
Min-Kyeong Kang <gangriming@naver.com>
2021-05-02 02:33:44 +0900

    트리부모찾기_dfs
    조금 더 분석해봐야 한다.

--- VS_Solution/AlgorithmSolve/BOJ_11725.cpp
+++ VS_Solution/AlgorithmSolve/BOJ_11725.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct node
 {
-	int num = -1;	// -1 = 없음
+	//bool visit = false;
+	//int connet = -1;
+	int num = -1;
 	node* parent = nullptr;
 };
+
+void find_parent(int index, vector<bool>* visit, vector<int>** tree, vector<int>* parent)
+{
+	(*visit)[index] = true;
+	
+	for (int i = 0; i < (*tree)[index].size(); ++i)
+	{
+		int next = (*tree)[index][i];
+
+		if (!(*visit)[next])	// 방문 안했으면
+		{
+			(*parent)[next] = index;
+			find_parent(next, visit, tree, parent);
+		}
+	}
+}
 
 int main()
 {
@@ -18,17 +37,37 @@ int main()
 	int count = 0;
 	cin >> count;
 
+	vector<int> *tree = new vector<int>[count + 1];	// 트리
+	vector<int> parent(count + 1);	// 부모
+	vector<bool> visit(count + 1, false);
+
+	for (int i = 0; i < count - 1; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+
+	find_parent(1, &visit, &tree, &parent);
+
+	for (int i = 2; i <= count; ++i)
+		cout << parent[i] << "\n";
+
+
+	delete[] tree;
 
 	// 되긴 하는데, 원하는 답이 아님.
+	
 	/*
-	node tree[100001];
+	node tree[100005];
 	tree[1].num = 1;
-
+	
 	for (int i = 0; i < count-1; ++i)
 	{
 		int a, b = 0;
 		cin >> a >> b;
-
+	
 		if (a == 1 || b == 1)	// 1의(루트 노드의) 자식인 경우
 		{
 			if (a != 1)
@@ -56,8 +95,9 @@ int main()
 			}
 		}
 	}
-
+	
 	for (int i = 0; i < count - 1; ++i)
-		cout << tree[i + 2].parent->num << endl;
+		if (!tree[i+2].parent)
+			cout << tree[i + 2].parent->num << "\n";
 	*/
 }
