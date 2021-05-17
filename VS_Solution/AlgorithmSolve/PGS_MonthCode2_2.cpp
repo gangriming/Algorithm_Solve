0276d82f8c6f47d6be7fdfe9e6496674cb38421b
Minkyeong <gangriming@naver.com>
2021-05-17 19:28:53 +0900

    PGS_2개이하..._비트연산
    아직 홀수 탐색에서 분전중..
    이거 하면서 비트에 대한 자료나 비트 마스킹 풀어볼 예정

--- VS_Solution/AlgorithmSolve/PGS_MonthCode2_2.cpp
+++ VS_Solution/AlgorithmSolve/PGS_MonthCode2_2.cpp
@@ -8,6 +8,8 @@ using namespace std;
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
 
+    // **기존 풀이**
+    /*
     for (int i = 0; i < numbers.size(); ++i)
     {
         string bit = bitset<20>(numbers[i]).to_string();
@@ -38,8 +40,62 @@ vector<long long> solution(vector<long long> numbers) {
             }
         }
     }
+    */
 
-    // 2개 테케가 시간초과뜸;
+    // **해설 보고 푼 풀이**
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        if (numbers[i] % 2 == 0)     // 짝수
+        {
+            /*
+            auto bit = bitset<20>(numbers[i]);
+
+            for (int i = 0; i < bit.size(); ++i)
+            {
+                if (!bit.test(i)) // 작은 수에서 0 이 나온다면
+                {
+                    bit.set(i, true);
+                    break;
+                }
+            }
+            answer.push_back(bit.to_ullong());
+            */
+
+            // 잘 생각해보면, 짝수의 경우에는 무조건 맨 오른쪽 비트(1을나타냄)가 0일테니,
+            // 1을 더해서 마지막 오른쪽 비트만 바꾸어준다면, 조건을 충족한다.
+            answer.push_back(numbers[i] + 1);
+        }
+        else                         // 홀수
+        {
+            // https://withhamit.tistory.com/564 참고
+            // 오른쪽에서부터 검색해, 제일 작은 0을 1로 바꾸고
+            // 그 다음에 나오는 1을 바꾼다.(없음 말고)
+            auto bit = bitset<20>(numbers[i]);
+
+            bool flag = false;
+            for (int i = 0; i < bit.size(); ++i)
+            {
+                if (!bit.test(i)) // 작은 수에서 0 이 나온다면
+                {
+                    bit.set(i, true);
+                    for (int j = i-1; j >= 0; --j)
+                    {
+                        if (bit.test(j)) // 그다음 작은 1을
+                        {
+                            bit.set(j, false);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag)
+                        break;
+                }
+            }
+            answer.push_back(bit.to_ullong());
+        }
+    }
+
+
     return answer;
 }
 
