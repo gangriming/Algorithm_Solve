bccfeb00553a0b157e0078e2530fe737f5fa758f
Minkyeong <gangriming@naver.com>
2021-05-17 20:45:54 +0900

    BOJ_막대기_수학,비트
    수학으로 풀어도 되는데
    간단한 문젠데 잘 이해가 안됨;;난독증인가

---
+++ VS_Solution/AlgorithmSolve/BOJ_1094.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+
+	auto xBit = bitset<7>(x);
+
+	// https://www.acmicpc.net/problem/1094
+	// 몇개의 막대를 풀로 붙여서 x를 만들 수 있는가?
+	int stick = 64;
+	int sum = 0;
+	while (sum != x)
+	{
+
+	}
+}
