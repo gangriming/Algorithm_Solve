fde267d3a360a61055a7a04ab314f500b716726c
Minkyeong <gangriming@naver.com>
2021-05-31 20:47:19 +0900

    BOJ_집합_비트마스킹(하는중)
    bitset이용해서 하니까 시간초과 뜸.
    그냥 원시적으로 & ^ 로 풀어야할듯?

--- VS_Solution/AlgorithmSolve/BOJ_11723.cpp
+++ VS_Solution/AlgorithmSolve/BOJ_11723.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <bitset>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
@@ -11,27 +11,84 @@ using namespace std;
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	//std::ios_base::sync_with_stdio(false);
+	//cin.tie(NULL);
+	//cout.tie(NULL);
 
 	int m;
 	cin >> m;
+
+	//cin.ignore();
 
 	auto bit = bitset<20>();
 
 	while (m)
 	{
+
+		/*
 		string s;
+		cin.ignore();		// 버퍼를 비워줘야함.
 		getline(cin, s);	// 공백이 있기 떄문에 getline 이용.
+		//cout << s;
 
 		istringstream ss(s);
 		string buf;
+
 		vector<string> v;
 		while (getline(ss, buf, ' '))
-		{
 			v.push_back(buf);
-			cout << buf << endl;
+		*/
+
+
+		// v[0] = 종류
+		// v[1] = 수
+
+		string s;
+		cin >> s;
+
+		if (s == "add")
+		{
+			string n;
+			cin >> n;
+
+			int num = atoi(n.c_str()) - 1;
+			bit[num] = 1;
+		}
+		else if (s == "remove")
+		{
+			string n;
+			cin >> n;
+
+			int num = atoi(n.c_str()) - 1;
+			bit[num] = 0;
+		}
+		else if (s == "check")
+		{
+			string n;
+			cin >> n;
+
+			int num = atoi(n.c_str()) - 1;
+			//if (bit.test(num))
+			//	cout << "1\n";
+			//else
+			//	cout << "0\n";
+			cout << bit[num] << "\n";
+		}
+		else if (s == "toggle")
+		{
+			string n;
+			cin >> n;
+
+			int num = atoi(n.c_str()) - 1;
+			bit.flip(num);
+		}
+		else if (s == "all")
+		{
+			bit.set();
+		}
+		else if (s == "empty")
+		{
+			bit.reset();
 		}
 
 		m--;
