d44c8ed98c03bc198a392ad2b3a8f9c3ccc77769
Minkyeong <gangriming@naver.com>
2024-02-14 03:20:33 +0900

    프로그래머스 코테 강의 6.  Dynamic Programming N으로 표현 - 문제의 답을 알아내기 위해 확인해야 하는 공간을, 문제를 진행하면서 동적으로 결정한다. - 주어진 최적화 문제를, `재귀적` 방식으로 보다 작게 나누어 부분문제를 푼다.(해를 조합해 전체의 해답)

---
+++ VS_Solution/AlgorithmSolve/PGS_Lecture_6.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+int solution(int N, int number)
+{
+	int answer = -1;
+
+	unordered_set<int> s[8];
+	int base = 0;
+	
+	for (int i = 0; i < 8; ++i)
+	{
+		base = 10 * base + 1;
+		s[i].insert(base * N);	// N번 연달아 썼을때 만들어지는 수 부터 넣어놓는다. // 5, 55, 555 ...
+	}
+
+	for (int i = 0; i < 8; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			for (auto& op1 : s[j])	// 왼쪽. A * B 에서 A
+			{
+				for (auto& op2 : s[i - j - 1])	// 오른쪽. A * B 에서 B 
+				{
+					// 사칙연산들 해준다.
+					s[i].insert(op1 + op2);
+					s[i].insert(op1 - op2);
+					s[i].insert(op1 * op2);
+					if(op2 != 0)
+						s[i].insert(op1 / op2);	
+				}
+			}
+		}
+
+		if (s[i].count(number) > 0)
+		{
+			answer = i + 1;
+			break;
+		}
+	}
+
+	return answer;
+}
