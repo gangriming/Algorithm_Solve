7fff0835c1fe6f6b0c041d552d1b647ecafebd20
Minkyeong <gangriming@naver.com>
2024-02-15 02:25:16 +0900

    BOJ_11729, 1781 - 예전에 틀렸었어서 한번 다시 보다가 같은 이유로 실패 ㅠㅜ

--- VS_Solution/AlgorithmSolve/BOJ_11729.cpp
+++ VS_Solution/AlgorithmSolve/BOJ_11729.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+#include <vector>
 #include <stack>
 
 using namespace std;
 
+vector<stack<int>> poll;
+int disk = 0;
+int movecnt = 0;
+
+// ^^:;
+void towermove(vector<stack<int>>& p)
+{
+	// 마지막 폴에 디스크가 다 올라왔다면
+	if (p[2].size() >= disk)
+		return;
+}
+
+
 int main()
 {
-	// 하노이 탑
-	int disk = 0;
-	cin >> disk;
+	// 무조건 3개의 장대.
+	for (int i = 0; i < 3; ++i)
+		poll.push_back(stack<int>());
 
-	stack<int> a, b, c;
+	// 하노이 탑
+	cin >> disk;
 	
+	// 첫 장대에 세팅
 	for(int i = disk; i > 0; --i)
-		a.push(i);
+		poll[0].push(i);
 	
+	towermove(poll);
 }
--- VS_Solution/AlgorithmSolve/BOJ_1781.cpp
+++ VS_Solution/AlgorithmSolve/BOJ_1781.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
 #include <queue>
+#include <unordered_map>
 #include <set>
 
 using namespace std;
-
-// 우선순위 큐 + 그리드
-// 데드라인 내에 최대한 할 수 있는 양 구하기
-
-// 단위 시간이 1이라서 단순 하나만 false인데
-// 시간이 다른 경우라면 앞뒤 연속까지 비교해가면서?
-// 미해결
-
-
-struct cmp
-{
-	bool operator()(pair<int, int>a, pair<int, int>b)
-	{
-	//	if (a.first == b.first)
-			return a.second < b.second;
-	//	else
-	//		return a.first > b.first;
-	}
-};
 
 int main()
 {
@@ -31,35 +13,20 @@ int main()
 
 	int n = 0;
 	cin >> n;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+	unordered_map<int, int> m;
 
 	while (n--)
 	{
 		int deadline, cup;
 		cin >> deadline >> cup;
-		pq.push(make_pair(deadline, cup));
+
+		if (m[deadline] < cup)
+			m[deadline] = cup;
 	}
 
-	bool dead[200001] = { false, };
-	int sum = 0;
-
-	/*
-	auto init = pq.top(); pq.pop();
-	int dead = init.first;
-	sum += init.second;
-
-	while (!pq.empty())
-	{
-		auto p = pq.top(); pq.pop();
-		if (dead != p.first)
-		{
-			// 달라진 초기값만 넣어준다
-			dead = p.first;
-			sum += p.second;
-		}
-	}
-	*/
-
+	long long sum = 0;
+	for (auto& value : m)
+		sum += value.second;
 
 	cout << sum << endl;
 }
