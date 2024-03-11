5ba9b3d6750d3a413b831a306a41dcd91c369ade
Minkyeong <gangriming@naver.com>
2024-03-12 02:51:52 +0900

    BOJ_11723 집합 https://www.acmicpc.net/problem/11723 - 비트마스킹 - string 변환에, getline과 istringstream을 사용하게되면 시간이 오래걸린다. (tc가 많이 없다면 사용해도 됨)

--- VS_Solution/AlgorithmSolve/BOJ_11723.cpp
+++ VS_Solution/AlgorithmSolve/BOJ_11723.cpp
@@ -2,91 +2,63 @@
 #include <sstream>
 #include <string>
 #include <bitset>
-#include <vector>
 
 using namespace std;
 
-// https://chbuljumeok1997.tistory.com/42
-// 위 링크는 string을 구분자로 자르는 다양한 방법
 
 int main()
 {
-	//std::ios_base::sync_with_stdio(false);
-	//cin.tie(NULL);
-	//cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 
-	int m;
+	cin.tie(NULL);
+
+	cout.tie(NULL);
+
+	int m = 0;
 	cin >> m;
-
-	//cin.ignore();
 
 	auto bit = bitset<20>();
 
 	while (m)
 	{
+		//string str = "";
+		//getline(cin, str);
 
-		/*
-		string s;
-		cin.ignore();		// 버퍼를 비워줘야함.
-		getline(cin, s);	// 공백이 있기 떄문에 getline 이용.
-		//cout << s;
+		//if (str.empty())
+		//	continue;
 
-		istringstream ss(s);
-		string buf;
+		//istringstream iss(str);
+		string command = "";
+		int number = 0;
 
-		vector<string> v;
-		while (getline(ss, buf, ' '))
-			v.push_back(buf);
-		*/
+		cin >> command;
+		if (command.empty())
+			continue;
 
+		if (command == "add" || command == "remove" || command == "check" || command == "toggle")
+			cin >> number;
 
-		// v[0] = 종류
-		// v[1] = 수
-
-		string s;
-		cin >> s;
-
-		if (s == "add")
+		if (command == "add")
 		{
-			string n;
-			cin >> n;
-
-			int num = atoi(n.c_str()) - 1;
-			bit[num] = 1;
+			bit[number-1] = 1;
 		}
-		else if (s == "remove")
+		else if (command == "remove")
 		{
-			string n;
-			cin >> n;
-
-			int num = atoi(n.c_str()) - 1;
-			bit[num] = 0;
+			bit[number-1] = 0;
 		}
-		else if (s == "check")
+		else if (command == "check")
 		{
-			string n;
-			cin >> n;
-
-			int num = atoi(n.c_str()) - 1;
-			//if (bit.test(num))
-			//	cout << "1\n";
-			//else
-			//	cout << "0\n";
-			cout << bit[num] << "\n";
+			cout << bit[number-1] << "\n";
 		}
-		else if (s == "toggle")
+		else if (command == "toggle")
 		{
-			string n;
-			cin >> n;
-
-			int num = atoi(n.c_str()) - 1;
-			bit.flip(num);
+			bit.flip(number-1);
 		}
-		else if (s == "all")
+		else if (command == "all")
 		{
 			bit.set();
 		}
-		else if (s == "empty")
+		else if (command == "empty")
 		{
 			bit.reset();
 		}
