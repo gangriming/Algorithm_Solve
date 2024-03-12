4e588022b84c8fde962f11aebfefaa9f8127eb3c
Minkyeong <gangriming@naver.com>
2024-03-12 23:25:12 +0900

    BOJ_11053 가장 긴 증가하는 부분 수열 https://www.acmicpc.net/problem/11053 - dp, LIS 문제. - https://seungkwan.tistory.com/8 이것처럼 좀 더 줄이는 방법도 있는 것 같다.

---
+++ VS_Solution/AlgorithmSolve/BOJ_11053.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>	
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n+1);
+	vector<int> dp(n+1);
+	int answer = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> v[i];
+		int check = 0;
+		for (int j = 1; j < i; ++j)
+		{
+			if (v[i] > v[j])	// 현재 체크하고 있는 값이, 이전 값들보다 크다면
+				check = max(check, dp[j]);
+		}
+		dp[i] = check + 1;
+		answer = max(answer, dp[i]);
+	}
+
+	cout << answer << "\n";
+}
